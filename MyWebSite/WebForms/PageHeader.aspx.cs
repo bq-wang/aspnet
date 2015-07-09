ef724c4aeb17015483fb48c770f5a102ada4006e
Joe Wang <joe.bq.wang@live.com>
2015-07-10 07:46:55 +0800

    dynamic generate page header (meta data )

---
+++ MyWebSite/WebForms/PageHeader.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.HtmlControls;
+
+namespace MyWebSite.WebForms
+{
+  public partial class PageHeader : System.Web.UI.Page
+  {
+    // Check methods
+    // you can open a Web page and then Inspect the page element.
+    protected void Page_Load(object sender, EventArgs e)
+    {
+      Page.Header.Title = "Dynamically titled page";
+
+      //  System.Web.UI.HtmlControls
+      HtmlMeta metaDescription = new HtmlMeta();
+
+      metaDescription.Content = "A great Website to lean .net";
+      metaDescription.Name = "Description";
+
+      // meta data labels
+
+      HtmlMeta metaKeywords = new HtmlMeta();
+
+      metaKeywords.Name = "keywords";
+      metaKeywords.Content = ".NET, C#, ASP.NET";
+      Page.Header.Controls.Add(metaKeywords);
+    }
+  }
+}
