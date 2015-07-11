4670cb399631c65e28d6477246062c087dd67d4e
Joe Wang <joe.bq.wang@live.com>
2015-07-11 21:45:55 +0800

    page trace header - key is the Trace="True" flag.

---
+++ MyWebSite/WebForms/TracePageFlow.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="TracePageFlow.aspx.cs" Inherits="MyWebSite.WebForms.TracePageFlow" Trace="true" %>
+<!-- the key attribute here is the Trace = "True" flag -->
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+      <asp:Label id="lbInfo" runat="server" EnableViewState="false">
+      </asp:Label>
+
+      <asp:Button ID="Button1" runat="server" Text="Button" OnClick="Button1_Click">
+      </asp:Button>
+    </div>
+    </form>
+</body>
+</html>
---
+++ MyWebSite/WebForms/TracePageFlow.aspx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MyWebSite.WebForms
+{
+  public partial class TracePageFlow : System.Web.UI.Page
+  {
+    protected void Page_Load(object sender, EventArgs e)
+    {
+      lbInfo.Text += "Page.Load event handled.<br />";
+
+      if (Page.IsPostBack)
+      {
+        lbInfo.Text += "<b>This is not the first time you've seen this page.</b> <br />";
+      }
+    }
+
+    protected void Page_Init(object sender, EventArgs e)
+    {
+      lbInfo.Text += "Page.Init event handled.<br />";
+    }
+
+    protected void Page_PreRender(object sender, EventArgs e)
+    {
+      lbInfo.Text += "Page.PreRender event handled.<br />";
+    }
+
+
+    protected void Page_Unload(object sender, EventArgs e)
+    {
+      lbInfo.Text += "Page.Unload event handled.<br />";
+    }
+
+    protected void Button1_Click(object sender, System.EventArgs e)
+    {
+      lbInfo.Text += "Button1.Click event handled.<br />";
+    }
+  }
+}
