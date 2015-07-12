1133aac260140c0e26cee5db92253535e8af722d
Joe Wang <joe.bq.wang@live.com>
2015-07-12 22:21:17 +0800

    ImageButton click event - web control events

---
+++ MyWebSite/ServerControls/ImageButtonClick.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ImageButtonClick.aspx.cs" Inherits="MyWebSite.ServerControls.ImageButtonClick" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+      <asp:Label ID="lblResult" runat="server" />
+      <asp:ImageButton ID="ImageButton1" runat="server" OnClick="ImageButton1_Click" Height="140px" Width="315px" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ MyWebSite/ServerControls/ImageButtonClick.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MyWebSite.ServerControls
+{
+  public partial class ImageButtonClick : System.Web.UI.Page
+  {
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void ImageButton1_Click(object sender, ImageClickEventArgs e)
+    {
+      lblResult.Text = "You clicked at (" + e.X.ToString() + ", " + e.Y.ToString() + ").";
+
+      if ((e.Y < 100) && (e.Y > 20) && (e.X > 20) && (e.X < 275))
+      {
+        lblResult.Text += "You clicked on the button surface";
+      }
+      else
+      {
+        lblResult.Text += "You clicked on the button border";
+      }
+    }
+  }
+}
