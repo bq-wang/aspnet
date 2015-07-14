fd3ddf389030e8b86a140d864bd3f2fdc99aaa1d
Joe Wang <joe.bq.wang@live.com>
2015-07-14 21:59:16 +0800

    calendar controls - DayRender styles, SelectionChanged events.

---
+++ MyWebSite/ServerControls/CalendarControl.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="CalendarControl.aspx.cs" Inherits="MyWebSite.ServerControls.CalendarControl" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    <!-- SelectionMode value other than Day , check SelectedDates rather than SelectedDate -->
+      <asp:Calendar runat="server" ID="Calendar1" ForeColor="Red" BackColor="LightYellow" OnSelectionChanged="Calendar1_SelectionChanged" SelectionMode="DayWeek" OnDayRender="Calendar1_DayRender"></asp:Calendar>
+
+      <asp:Label ID="lblDates" runat="server" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ MyWebSite/ServerControls/CalendarControl.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MyWebSite.ServerControls
+{
+  public partial class CalendarControl : System.Web.UI.Page
+  {
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Calendar1_SelectionChanged(object sender, EventArgs e)
+    {
+      // if SelectionMode is other than Day, check SelectedDates rather than SelectedDate
+      //lblDates.Text = "You selected: " + Calendar1.SelectedDate.ToLongDateString();
+
+
+      lblDates.Text = "You selected these dates: <br />";
+
+      foreach (DateTime dt in Calendar1.SelectedDates)
+      {
+        lblDates.Text += dt.ToLongDateString() + "<br />";
+      }
+    }
+
+    protected void Calendar1_DayRender(object sender, DayRenderEventArgs e)
+    {
+      if (e.Day.IsWeekend)
+      {
+        e.Cell.BackColor = System.Drawing.Color.Green;
+        e.Cell.ForeColor = System.Drawing.Color.Yellow;
+        e.Day.IsSelectable = false;
+      }
+    }
+  }
+}
