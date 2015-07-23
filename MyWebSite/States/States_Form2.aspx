39a9d6c5f5c62ccea91dc3c0e80d0dffa7df613f
Joe Wang <joe.bq.wang@live.com>
2015-07-23 22:12:11 +0800

    ViewState can save object (rather than just simple object)

---
+++ MyWebSite/States/States_Form2.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="States_Form2.aspx.cs" Inherits="MyWebSite.States.States_Form2" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    <table id="Table1" runat="server">
+
+    <tr><td><asp:Label ID="Label1" runat="server" Text="Description" /></td><td><asp:Label ID="Label2" runat="server" Text="Value" /></td></tr>
+
+    <tr><td><asp:Label ID="Label3" runat="server" Text="Name" /></td><td><asp:TextBox ID="txtName" runat="server" /></td></tr>
+
+    <tr><td><asp:Label ID="Label4" runat="server" Text="IF" /></td><td><asp:TextBox ID="txtID" runat="server" /></td></tr>
+
+    <tr><td><asp:Label ID="Label5" runat="server" Text="Age" /></td><td><asp:TextBox ID="txtAge" runat="server" /></td></tr>
+
+    <tr><td><asp:Label ID="Label6" runat="server" Text="E-Mail" /></td><td><asp:TextBox ID="txtEmail" runat="server" /></td></tr>
+
+    <tr><td><asp:Label ID="Label7" runat="server" Text="Password" /></td><td><asp:TextBox ID="txtPassword" runat="server" /></td></tr>
+ 
+    <tr><td><asp:Button ID="btnSave" runat="server" OnClick="btnSaveClick" Text="save"/></td><td><asp:Button ID="btnRestore" runat="server" OnClick="btnRestoreClick" Text="Restore" /></td><td><asp:Button ID="btnDisplay" runat="server" OnClick="btnDisplayClick" Text="Display"/></td></tr>
+    </table>
+
+    <asp:Label ID="lblResult" runat="server" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ MyWebSite/States/States_Form2.aspx.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MyWebSite.States
+{
+  /// <summary>
+  /// this example compared to the Form1 - to demonstrate that you can save object (rather than simple string) into ViewStates.
+  /// </summary>
+  public partial class States_Form2 : System.Web.UI.Page
+  {
+    protected void Page_Load(object sender, EventArgs e)
+    {
+    }
+
+    protected void btnSaveClick(object sender, EventArgs e)
+    {
+      Dictionary<string, string> textToSave = new Dictionary<string, string>();
+
+      SaveAllText(Table1.Controls, textToSave, true);
+
+      ViewState["ControlText"] = textToSave;
+    }
+
+    private void SaveAllText(ControlCollection controls, Dictionary<string, string> textToSave, bool saveNested)
+    {
+      foreach (Control control in controls)
+      {
+        // add the text into the dictionary
+        if (control is TextBox)
+        {
+          textToSave.Add(control.ID, ((TextBox)control).Text);
+        }
+
+        if (control.Controls != null && saveNested)
+        {
+          SaveAllText(control.Controls, textToSave, saveNested);
+        }
+      }
+    }
+
+    private void RestoreAllText(ControlCollection controls, Dictionary<string, string> textToSave, bool saveNested)
+    {
+      foreach (Control control in controls)
+      {
+        if (control is TextBox)
+        {
+          if (textToSave.ContainsKey(control.ID) != null)
+            ((TextBox)control).Text = textToSave[control.ID];
+        }
+
+        if (control.Controls != null && saveNested)
+        {
+          RestoreAllText(control.Controls,  textToSave, saveNested);
+        }
+      }
+    }
+
+
+    protected void btnRestoreClick(object sender, EventArgs e)
+    {
+      if (ViewState["ControlText"] != null)
+      {
+        Dictionary<string, string> textToSave = ViewState["ControlText"] as Dictionary<string, string>;
+        if (textToSave != null)
+        {
+          RestoreAllText(Table1.Controls, textToSave, true);
+        }
+      }
+    }
+
+    protected void btnDisplayClick(object sender, EventArgs e)
+    {
+      if (ViewState["ControlText"] != null)
+      {
+        // get the dictionary 
+        Dictionary<string, string> savedText = (Dictionary<string, string>)ViewState["ControlText"];
+
+        lblResult.Text = "";
+        foreach (KeyValuePair<string, string> item in savedText)
+        {
+          lblResult.Text += (string)item.Key + "=" + (string)item.Value + "<br />";
+        }
+      }
+    }
+  }
+}
