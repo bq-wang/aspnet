95ee99f779fe5ff85d643a57cb67c89268b5008f
Joe Wang <joe.bq.wang@live.com>
2015-07-25 15:38:08 +0800

    simple cookie - set/get/remove cookies

---
+++ MyWebSite/States/SimpleCookie.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="SimpleCookie.aspx.cs" Inherits="MyWebSite.States.SimpleCookie" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+      <!-- this page will create a simple cookie to by code to show how -->
+
+      <asp:Button ID="cmdSetCookie" runat="server" OnClick="cmdSetCookieClick" Text="setCookie" />
+      <asp:Button ID="cmdShowCookie" runat="server" OnClick="cmdShowCookieClick" Text="showCookie" />
+      <asp:Button ID="cmdRemoveCookie" runat="server" OnClick="cmdRemoveCookieClick" Text="removeCookie" />
+      <asp:Label ID="lblInfo" runat="server" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ MyWebSite/States/SimpleCookie.aspx.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MyWebSite.States
+{
+  public partial class SimpleCookie : System.Web.UI.Page
+  {
+    // you can find the location to the Chrome cookie location 
+    // at %userprofile%\AppData\Local\Google\Chrome\User Data\Default
+    protected void Page_Load(object sender, EventArgs e)
+    {
+    }
+
+    protected void cmdSetCookieClick(object sender, EventArgs e)
+    {
+      SetCookie();
+
+      lblInfo.Text = "Set cookie done";
+    }
+
+    protected void SetCookie()
+    {
+      HttpCookie cookie = new HttpCookie("Preferences");
+      // set one of the values
+
+      cookie["LanguagePref"] = "English";
+
+      // add another value 
+      cookie["Country"] = "US";
+
+      // add the cookie back to the current web response
+
+      Response.Cookies.Add(cookie);
+
+      cookie.Expires = DateTime.Now.AddYears(1);
+    }
+
+    protected void cmdRemoveCookieClick(object sender, EventArgs e)
+    {
+      RemoveCookie();
+
+      lblInfo.Text = "Cookie removed";
+    }
+
+    protected void RemoveCookie()
+    {
+      HttpCookie cookie = new HttpCookie("Preferences");
+      cookie.Expires = DateTime.Now.AddDays(-1);
+      Response.Cookies.Add(cookie);
+    }
+
+
+    protected void cmdShowCookieClick(object sender, EventArgs e)
+    {
+      ShowCookie();
+    }
+
+    protected void ShowCookie()
+    {
+      // find the cookie
+      HttpCookie cookie = Request.Cookies["Preferences"];
+
+      if (cookie != null)
+      {
+        string language = cookie["LanguagePref"];
+        lblInfo.Text = language;
+      }
+      else
+      {
+        lblInfo.Text = "cookie not found";
+      }
+
+    }
+
+  }
+}
