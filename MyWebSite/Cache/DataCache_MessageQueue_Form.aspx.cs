b6b1588f061ba9f568519c3f50c74497be5b515d
Joe Wang <joe.bq.wang@live.com>
2015-08-25 22:31:11 +0800

    message queue Data cache dependencies.

---
+++ MyWebSite/Cache/DataCache_MessageQueue_Form.aspx.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Messaging;
+using System.Collections;
+
+namespace MyWebSite.Cache
+{
+  public partial class DataCache_MessageQueue_Form : System.Web.UI.Page
+  {
+    // to use the windows message queue, you will need to install the windows message queue feature, 
+    // you can turn this on by 
+    // program, window features then find Window Message queue and expand to tick Window Message queeu core.
+    //
+    // seems that @".\Private$\TEstQueue" has been used somewhere.
+    //
+    private string queueName = @".\Private$\TestQueue1";
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+      if (!Page.IsPostBack)
+      {
+        MessageQueue queue;
+
+        if (MessageQueue.Exists(queueName))
+        {
+          queue = new MessageQueue(queueName);
+        }
+        else
+        {
+          queue = MessageQueue.Create(queueName);
+        }
+
+        lblInfo.Text += "Create dependencies item...<br />";
+        Cache.Remove("Item");
+        MessageQueueCacheDependency  dependency = new MessageQueueCacheDependency(queueName);
+        string item = "Dependent cached item";
+        lblInfo.Text += "Adding dependent item<br />";
+        Cache.Insert("Item", item, dependency);
+      }
+    }
+
+    protected void cmdModify_OnClick(object sender, EventArgs e)
+    {
+      MessageQueue queue = new MessageQueue(queueName);
+      // you can send not only a string ,but also a custom object
+      queue.Send("Invalidated!");
+      lblInfo.Text += "Message Sent<br />";
+    }
+
+    protected void cmdCheckitems_OnClick(object sender, EventArgs e)
+    {
+      string itemList = "";
+      foreach (DictionaryEntry item in Cache)
+      {
+        itemList += item.Key.ToString() + " ";
+      }
+      lblInfo.Text += "<br />Found: " + itemList + "<br />";
+
+    }
+  }
+}
---
+++ MyWebSite/Cache/MessageQueueCacheDependency.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Caching;
+using System.Messaging;
+using System.Threading;
+
+// we need to add the reference to the System.Messaging.dll
+namespace MyWebSite.Cache
+{
+  public class MessageQueueCacheDependency : CacheDependency
+  {
+    private MessageQueue queue;
+
+    public MessageQueueCacheDependency(string queueName)
+    {
+      queue = new MessageQueue(queueName);
+
+      // on another thread
+      WaitCallback callback = new WaitCallback(WaitForMessage);
+
+      ThreadPool.QueueUserWorkItem(callback);
+    }
+
+    public void WaitForMessage(object state)
+    {
+      // check the resource 
+
+      /*
+      Message msg = null;
+      while (true)
+      {
+        msg = queue.Receive();
+        if (!string.IsNullOrEmpty(msg.Label))
+        {
+          break;
+        }
+      }
+      */
+      Message msg = queue.Receive();
+      // block until the message sent to the queue 
+      base.NotifyDependencyChanged(this, EventArgs.Empty);
+    }
+
+  }
+}
