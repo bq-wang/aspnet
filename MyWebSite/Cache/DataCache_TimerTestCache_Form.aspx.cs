16429288f6c42d93ce09ff02d139bb99f287d4b2
Joe Wang <joe.bq.wang@live.com>
2015-08-25 21:40:13 +0800

    customer cache dependency - TimerTestCache and its test form.

---
+++ MyWebSite/Cache/DataCache_TimerTestCache_Form.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Collections;
+
+namespace MyWebSite.Cache
+{
+  public partial class DataCache_TimerTestCache_Form : System.Web.UI.Page
+  {
+    protected void Page_Load(object sender, EventArgs e)
+    {
+      if (!Page.IsPostBack)
+      {
+        var item = "TestString";
+        TimerTestCacheDependency dependency = new TimerTestCacheDependency();
+        Cache.Insert("MyItem", item, dependency);
+      }
+    }
+    protected void cmdCheckitems_OnClick(object sender, EventArgs e)
+    {
+      string itemList = "";
+      foreach (DictionaryEntry item in Cache)
+      {
+        itemList += item.Key.ToString() + " ";
+      }
+      lblInfo.Text += "<br />Found: " + itemList + "<br />";
+    }
+  }
+}
---
+++ MyWebSite/Cache/TimerTestCacheDependency.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyWebSite.Cache
+{
+  public class TimerTestCacheDependency : System.Web.Caching.CacheDependency
+  {
+    private System.Threading.Timer timer;
+
+    private int pollTime = 5000;
+
+    public TimerTestCacheDependency()
+    {
+      timer = new System.Threading.Timer(new System.Threading.TimerCallback(CheckDependencyCallback),
+        this,
+        0,
+        pollTime);
+    }
+    private int count;
+    private void CheckDependencyCallback(object sender)
+    {
+
+      count++;
+      if (count > 4)
+      {
+        base.NotifyDependencyChanged(this, EventArgs.Empty);
+        timer.Dispose();
+      }
+    }
+  }
+}
