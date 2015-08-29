c80a05dba70ec429296f2b3e5f4d88360cef8c9e
Joe Wang <joe.bq.wang@live.com>
2015-08-29 22:25:36 +0800

    Secures to multiple users - unique files names

---
+++ MyWebSite/FileSystems/Secure2MultipleUsers_Form.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Secure2MultipleUsers_Form.aspx.cs" Inherits="MyWebSite.FileSystems.Secure2MultipleUsers_Form" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+      <!-- 1. the page will show a technique
+      1.2. to get a unique file name
+      1.2.  use the unique file name to write your own log -->
+      <asp:Button runat="server" ID="cmdPostBack" Text="Post Back" />
+      <asp:Button runat="server" ID="cmdRead" OnClick="cmdRead_Click" Text="Read Log" />
+      <asp:Button runat="server" ID="cmdDelete" OnClick="cmdDelete_Click" Text="Delete Log" />
+      <asp:Label runat="server" ID="lblInfo"></asp:Label>
+    </div>
+    </form>
+</body>
+</html>
---
+++ MyWebSite/FileSystems/Secure2MultipleUsers_Form.aspx.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+using System.Text;
+
+namespace MyWebSite.FileSystems
+{
+  public partial class Secure2MultipleUsers_Form : System.Web.UI.Page
+  {
+    protected void Page_Load(object sender, EventArgs e)
+    {
+      if (!Page.IsPostBack)
+      {
+        Log("Page loaded for the first time.");
+      }
+      else
+      {
+        Log("Page Posted back.");
+      }
+    }
+
+    private void Log(string message)
+    {
+      FileMode mode;
+      if (ViewState["LogFile"] == null)
+      {
+        ViewState["LogFile"] = GetFileName();
+        mode = FileMode.Create;
+      }
+      else
+      {
+        // Append to existing file
+        mode = FileMode.Append;
+      }
+
+      string fileName = (string)ViewState["LogFile"];
+      using (FileStream fs = new FileStream(fileName, mode))
+      {
+        StreamWriter w = new StreamWriter(fs);
+        w.WriteLine(DateTime.Now);
+        w.WriteLine(message);
+        w.WriteLine();
+        w.Close();
+      }
+    }
+
+    protected string GetFileName()
+    {
+      string fileName = "User." + Guid.NewGuid().ToString();
+      return Path.Combine(Request.PhysicalApplicationPath, fileName);
+    }
+
+    protected void cmdRead_Click(object sender, EventArgs e)
+    {
+      if (ViewState["LogFile"] != null)
+      {
+        StringBuilder log = new StringBuilder();
+
+        // get the files to add html line breaker line by line
+        string fileName = (string)ViewState["LogFile"];
+        using (FileStream fs = new FileStream(fileName, FileMode.Open))
+        {
+          StreamReader r = new StreamReader(fs);
+          string line;
+          do{
+            line = r.ReadLine();
+            if (line != null)
+            {
+              log.Append(line + "<br />");
+            }
+          } while (line != null);
+          r.Close();
+        }
+
+         lblInfo.Text = log.ToString();
+        
+      }
+      else
+      {
+        lblInfo.Text = "There is no log file.";
+      }
+    }
+
+    protected void cmdDelete_Click(object sender, EventArgs e)
+    {
+      if (ViewState["LogFile"] != null)
+      {
+        File.Delete((string)ViewState["LogFile"]);
+        ViewState["LogFile"] = null;
+      }
+    }
+  }
+}
