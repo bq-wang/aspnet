7437d12d64c0e3fb7aa89b6ff255ec4df766f353
bq-wang <redalert2c@live.com>
2015-09-12 17:21:03 +0800

    Autogen with LinqNorthWindDB.dbml (Database markup language)

---
+++ MyWebSite/LinqSql/AutoGenLinqDb_Form.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using MyWebSite.LinqSql.AutoGen;
+
+namespace MyWebSite.LinqSql
+{
+    public partial class AutoGenLinqDb_Form : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            AutoGenLinqNorthwindDB db = new AutoGenLinqNorthwindDB();
+            StringBuilder sb = new StringBuilder();
+            foreach (Customers customer in db.GetCustomers())
+            {
+                sb.Append(customer.CompanyName);
+                sb.Append("<br />");
+
+                foreach (Orders order in customer.Orders)
+                {
+                    sb.Append(order.OrderID.ToString());
+                    sb.Append(" - ");
+                    sb.Append(order.OrderDate.Value.ToString());
+                    sb.Append("<br />");
+                }
+
+                sb.Append("<hr /><br />");
+            }
+
+
+            lblInfo.Text = sb.ToString();
+
+        }
+    }
+}
---
+++ MyWebSite/LinqSql/AutoGenLinqNorthwindDB.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Linq;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using MyWebSite.LinqSql.AutoGen;
+
+namespace MyWebSite.LinqSql
+{
+    public class AutoGenLinqNorthwindDB
+    {
+        private DataContext dataContext;
+        #region Ctor(s) 
+        public AutoGenLinqNorthwindDB() : this (WebConfigurationManager.ConnectionStrings["NorthWind"].ConnectionString)
+        {
+
+        }
+        public AutoGenLinqNorthwindDB(string connection)
+        {
+            dataContext = new DataContext(connection);
+        }
+        #endregion
+        
+        public List<Customers> GetCustomers()
+        {
+            return dataContext.GetTable<Customers>().ToList();
+        }
+    }
+}
