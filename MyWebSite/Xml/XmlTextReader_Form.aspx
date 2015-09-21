c37de232f9e46574ec3acb1b2dd477ba7d90b662
bq-wang <redalert2c@live.com>
2015-09-21 22:35:21 +0800

    Xml data (DvdList.xml) and Schema file Product.xsd first check in; XmlTextReader example.

---
+++ MyWebSite/Xml/XmlTextReader_Form.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="XmlTextReader_Form.aspx.cs" Inherits="MyWebSite.Xml.XmlTextReader_Form" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <!-- in this file, we will show 
+        1. how to read from xml with xmlTextReader 
+    -->
+    <form id="form1" runat="server">
+    <div>
+        <asp:Label runat="server" ID="lblXml" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ MyWebSite/Xml/XmlTextReader_Form.aspx.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml;
+
+namespace MyWebSite.Xml
+{
+    public partial class XmlTextReader_Form : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                ReadXML();
+            }
+
+        }
+
+
+        private void ReadXML()
+        {
+            string xmlFile = Server.MapPath("DvdList.xml");
+
+            // create the reader 
+            XmlTextReader reader = new XmlTextReader(xmlFile);
+            StringBuilder str = new StringBuilder();
+            
+            // iterate through the nodes 
+            while (reader.Read()) 
+            {
+                switch (reader.NodeType) 
+                {
+                    case XmlNodeType.XmlDeclaration:
+                        str.Append("XML Declaration: <b>");
+                        str.Append(reader.Name);
+                        str.Append("  ");
+                        str.Append(reader.Value);
+                        str.Append("</b><br />");
+                        break;
+                    case XmlNodeType.Element:
+                        str.Append("Element : <b>");
+                        str.Append(reader.Name);
+                        str.Append("</b><br />");
+                        break;
+                    case XmlNodeType.Text:
+                        str.Append(" - Value: <b>");
+                        str.Append(reader.Value);
+                        str.Append("</b><br />");
+                        break;
+
+                }
+
+                if (reader.HasAttributes)
+                {
+                    while (reader.MoveToNextAttribute()) 
+                    {
+                        str.Append("  - Attribute: <b>");
+                        str.Append(reader.Name);
+                        str.Append("<b> Value: <b>");
+                        str.Append(reader.Value);
+                        str.Append("</b><br />");
+                    }
+                }
+            }
+            reader.Close();
+            lblXml.Text = str.ToString();
+        }
+    }
+}
